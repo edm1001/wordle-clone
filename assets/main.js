7b4f4057a5503238d290d5638b3cd05de978718e
edm1001 <edmer_franciz@yahoo.com>
2023-06-21 11:20:01 -0500

    make keyboard responsive

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {   
     createBoxes()    
 
+    const guessedWords = [[]];
+    const availableSpace = 1;
 
+    // responsive keyboard
+    const keys = document.querySelectorAll('.keyboard-row button') //targetskeys
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({target}) => {
+            const key = target.getAttribute("data-key")
+            updateGuessedWords(letter)
+            console.log(key)
+        }
+        
+    }
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length
+        return guessedWords[numberOfGuessedWords -1]
+    } 
+
+
+    // keep the letters that are clicked
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr()
+
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter)
+
+            const availableSpaceEl = document.getElementById()
+        }
+    } 
+
+
+    // create boxed for the letters
     function createBoxes() {
         const gameBoard = document.getElementById("board");
 
@@ -12,4 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square)
         }
     }
+
+
 })

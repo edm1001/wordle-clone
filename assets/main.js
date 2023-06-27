5162723eec7def957082bdf8d9e0f5aa34dc0f8a
edm1001 <edmer_franciz@yahoo.com>
2023-06-27 12:50:22 -0500

    add function to get req

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const guessedWords = [[]];
     let availableSpace = 1;
 
-     let word = "books"
+     let word;
      let guessedWordCount = 0;
 
     // responsive keyboard
     const keys = document.querySelectorAll('.keyboard-row button') //targetskeys
 
+    function getNewWord() {
+        fetch(
+            `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
+            {
+                method:"GET",
+                headers: {
+                    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+                    "x-rapidapi-key": "<Key Here>",
+                }
+            }
+        )
+        .then((response) => {
+            return response.json();
+        })
+        .then((res) => {
+            word = res.word;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
 
     function getCurrentWordArr() {
         const numberOfGuessedWords = guessedWords.length;
